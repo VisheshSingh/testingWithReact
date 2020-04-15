7938b9bc694cbc9472795c1530e506bda719e185
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-14 21:37:14 -0500

    WIP boilerplate shared button ✅

---
+++ src/components/button/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class SharedButton extends Component {
+  static propTypes = {};
+
+  render() {
+    return <button></button>;
+  }
+}
+
+export default SharedButton;
