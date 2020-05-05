2b5adb816b78ef09d855fd54ab7fc117f7b5d21e
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-04 22:24:37 -0500

    component, title, desc test render 😴

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class ListItem extends Component {
-    static propTypes = {
-        title: PropTypes.string,
-        desc: PropTypes.string
+  static propTypes = {
+    title: PropTypes.string,
+    desc: PropTypes.string,
+  };
+
+  render() {
+    const { title, desc } = this.props;
+    if (!title) {
+      return null;
     }
 
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+    return (
+      <div data-test='listComponent'>
+        <h2 data-test='title'>{title}</h2>
+        <p data-test='desc'>{desc}</p>
+      </div>
+    );
+  }
 }
 
-export default ListItem
+export default ListItem;
--- src/components/ListItem/ListItem.test.js
+++ src/components/ListItem/ListItem.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { findByAttr, checkProps } from '../../../utils/';
+import { findByAttr, checkProps, findByTestAttr } from '../../../utils/';
 import ListItem from './ListItem';
+import { shallow } from 'enzyme';
 
 describe('ListItem component', () => {
   describe('Checking PropTypes', () => {
@@ -12,6 +13,29 @@ describe('ListItem component', () => {
 
       const propsErr = checkProps(ListItem, expectedProps);
       expect(propsErr).toBeUndefined();
+    });
+  });
+
+  describe('Component renders', () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {
+        title: 'Title',
+        desc: 'Description',
+      };
+      wrapper = shallow(<ListItem {...props} />);
+    });
+    test('Should render wrapper without errors', () => {
+      const component = findByTestAttr(wrapper, 'listComponent');
+      expect(component.length).toBe(1);
+    });
+    test('Should render title without errors', () => {
+      const title = findByTestAttr(wrapper, 'title');
+      expect(title.length).toBe(1);
+    });
+    test('Should render desc without errors', () => {
+      const desc = findByTestAttr(wrapper, 'desc');
+      expect(desc.length).toBe(1);
     });
   });
 });
