30db77e9f51179767be4cf22fa4e7ccb0f5d0226
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-06 23:15:18 -0500

    moxios

---
+++ src/_integrationTests/test.js
@@ -0,0 +1,43 @@
+import { testStore } from '../../utils';
+import moxios from 'moxios';
+import { fetchPost } from '../actions/FetchPostsAction';
+
+describe('fetch posts action', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test('store is updated correctly', () => {
+    const expectedState = [
+      {
+        title: 'Title1',
+        body: 'Body1',
+      },
+      {
+        title: 'Title2',
+        body: 'Body2',
+      },
+      {
+        title: 'Title3',
+        body: 'Body3',
+      },
+    ];
+
+    const store = testStore();
+
+    const request = moxios.requests.mostRecent();
+    request.respondWith({
+      status: 202,
+      response: expectedState,
+    });
+
+    return store.dispatch(fetchPost()).then(() => {
+      const newState = store.getState();
+      expectedState(newState.posts).toBe(expectedState);
+    });
+  });
+});
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,7 @@
 import checkPropTypes from 'check-prop-types';
+import { applyMiddleware, createStore } from 'redux';
+import RootReducer from '../src/reducers';
+import { middlewares } from '../src/createStore';
 
 export const findByTestAttr = (component, attr) => {
   const wrapper = component.find(`[data-test='${attr}']`);
@@ -13,4 +16,9 @@ export const checkProps = (component, expectedProps) => {
     component.name
   );
   return propErr;
+};
+
+export const testStore = (initialState) => {
+  const createStoreMiddleware = applyMiddleware(...middlewares)(createStore);
+  return createStoreMiddleware(RootReducer, initialState);
 };
